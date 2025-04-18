8a17e122f216c113c4af19183dc43f257959a5b2
Atheee-Stack <grosseetoile2025@gmail.com>
2025-04-18 19:44:57 +0800

    bun init
    bun init

---
+++ apps/api-gateway/src/global.d.ts
@@ -0,0 +1,7 @@
+// apps/api-gateway/src/global.d.ts
+declare module "repl";
+declare module "perf_hooks";
+declare module "async_hooks";
+declare module "http2";
+declare module "dgram";
+declare module "stream/web";
